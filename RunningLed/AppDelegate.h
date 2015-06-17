710e3a449062be68393d7c4639dbe98c8fc5365b
tubjng <mac@macs-MacBook-Pro.local>
2015-06-17 15:08:11 +0700

    Initial Commit

---
+++ RunningLed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunningLed
+//
+//  Created by tubjng on 6/17/15.
+//  Copyright (c) 2015 tubjng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunningLed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunningLed
+//
+//  Created by tubjng on 6/17/15.
+//  Copyright (c) 2015 tubjng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
